64ec9b30b1cddbe7f8f0ff90c52007f1e82f0db8
Martin Vejmelka <vejmelkam@gmail.com>
2014-01-28 19:40:59 -0700

    scrape_stations renamed to scrape_station

--- retrieve_observations.sh
+++ retrieve_observations.sh
@@ -5,5 +5,5 @@ STATIONS=`grep -v "^#" $1`
 for S in $STATIONS ;
 do
     echo "Processing $S ..."
-    python scrape_stations.py -c $S -i 24 -t $2 dl
+    python scrape_station.py -c $S -i 24 -t $2 dl
 done
