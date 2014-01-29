4c88728fd2429efcccfb69e45bc934d19db68ba4
Martin Vejmelka <vejmelkam@gmail.com>
2014-01-28 19:58:35 -0700

    added usage to retrieve_observations.sh

--- retrieve_observations.sh
+++ retrieve_observations.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+if [ $# -ne 2 ]; then
+  echo "usage: retrieve_observations.sh <station-list> <timestamp>"
+  echo "       <timestamp> must be formatted as YYYY-MM-DD_HH:mmm"
+  exit 2
+fi
+
+
 STATIONS=`grep -v "^#" $1`
 
 for S in $STATIONS ;
